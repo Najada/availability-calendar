961114494d49bad9f53c9a749b8000748699f57f
Najada kambo <najadakambo@gmail.com>
2021-12-18 23:57:31 +0100

    prepare events layout and relationship between user/events

--- app/Http/Livewire/AvailabilityCalendar.php
+++ app/Http/Livewire/AvailabilityCalendar.php
@@ -2,40 +2,26 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\User;
-use Livewire\Component;
+use App\Models\Event;
 use Asantibanez\LivewireCalendar\LivewireCalendar;
-use Carbon\Carbon;
 use Illuminate\Support\Collection;
 
 class AvailabilityCalendar extends LivewireCalendar
 {
-    // public function render()
-    // {
-    //     return view('livewire.availability-calendar');
-    // }
-
-
-    public function events() : Collection
+    public function events(): Collection
     {
-        if ($this->startsAt->month == 12) {
-        return collect([
-            [
-                'id' => 1,
-                'title' => 'Breakfast',
-                'description' => 'Pancakes! 🥞',
-                'date' => Carbon::today(),
-            ],
-            [
-                'id' => 2,
-                'title' => 'Meeting with Pamela',
-                'description' => 'Work stuff',
-                'date' => Carbon::tomorrow(),
-            ],
-        ]);
-    }
-    
-    return collect([]);
+
+        return Event::whereDate('date', '>=', $this->gridStartsAt)
+            ->whereDate('date', '<=', $this->gridEndsAt)
+            ->get()
+            ->map(function (Event $model) {
+                return [
+                    'id' => $model->id,
+                    'title' => 'title',
+                    'description' => 'notes',
+                    'date' => $model->date,
+                ];
+            });
     }
 
     public function goToPreviousMonth()
@@ -49,21 +35,13 @@ class AvailabilityCalendar extends LivewireCalendar
 
     public function onDayClick($year, $month, $day)
     {
-        dd(1);
-        // $this->isModalOpen = true;
-
-        // $this->resetNewAppointment();
-
-        // $this->newAppointment['scheduled_at'] = Carbon::today()
-        //     ->setDate($year, $month, $day)
-        //     ->format('Y-m-d');
+        $event = null;
+        if ($event = auth()->user()->events()->where('date', "{$year}-{$month}-{$day}")->first()) {
+            $event->delete();
+        } else {
+            auth()->user()->events()->create([
+                'date' => "{$year}-{$month}-{$day}"
+            ]);
+        }
     }
-
-   
-    // public function render()
-    // {
-    //     return parent::render()->with([
-    //         'events' => $this->events()
-    //     ]);
-    // }
 }
