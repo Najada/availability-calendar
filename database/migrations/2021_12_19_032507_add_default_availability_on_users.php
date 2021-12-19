8936d7b9d3305d721665dac86f528250889ed3ad
Najada kambo <najadakambo@gmail.com>
2021-12-20 00:07:36 +0100

    cleaning

--- database/migrations/2021_12_19_032507_add_default_availability_on_users.php
+++ database/migrations/2021_12_19_032507_add_default_availability_on_users.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class AddDefaultAvailabilityOnUsers extends Migration
 {
@@ -14,8 +16,18 @@ class AddDefaultAvailabilityOnUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-           $table->tinyInteger('is_available')->default(true)->after('remember_token');
+            $table->tinyInteger('is_available')->default(true)->after('remember_token');
         });
+
+        User::create(
+            [
+                'name' => 'user',
+                'email' => 'user@gmail.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ]
+        );
     }
 
     /**
